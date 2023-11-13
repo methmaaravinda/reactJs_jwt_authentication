c2a3d25587391e544577adb56362fee01b507569
dilshanAravinda <methmaaravinda@gmail.com>
2023-11-13 16:50:21 +0530

    add users route

--- src/servicers/queries.js
+++ src/servicers/queries.js
@@ -8,8 +8,8 @@ const logingQuery=(loginData)=>{
     return axiosPrivate.post(LOGIN_URL,{user:loginData})
 };
 const signUpQuery=(singUpData)=>{
-    // return axiosPrivate.post(SIGNUP_URL,{user:singUpData})
-    return axiosPrivate.get(SIGNUP_URL)
+    return axiosPrivate.post(SIGNUP_URL,{user:singUpData})
+    // return axiosPrivate.get(SIGNUP_URL)
 };
 const usersQuery=()=>{
     return axiosPrivate.get(USERS_URL)
